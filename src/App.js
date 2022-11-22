c84a5f00f9f51cadca209165cf6a9ae02d437d47
fizuli <guliyeffizuli20@gmail.com>
2022-11-22 15:32:02 +0400

    task3_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
+import Profile from "./Profile";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+    render(){
+        return (
+            <>
+            <Profile ad="Fizuli" soyad="Quliyev" email="guliyeffizuli20" adres="Bine" info="Çalışqan" />
+            <Profile ad="Balash" soyad="Huseynov" email="huseynovbalash07" adres="Memar Ecemi.M" info="Tenbel" />
+            <Profile ad="Orxan" soyad="Ismayilov" email="orxanismayilov" adres="Genclik.M" info="Oxuyan" />
+            <Profile ad="Mumin" soyad="Hemidov" email="hemidovmumin28" adres="Ehmedli.M" info="Starosta" />
+            </>
+        )
+    }
 }
-
-export default App;
